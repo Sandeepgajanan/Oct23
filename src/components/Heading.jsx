9619cc0ab2ecbd58aabf6e8659063bf40a26e600
Sandeepgajanan <beingsandeepg@gmail.com>
2024-10-25 15:04:53 +0530

    some minute changes added

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Heading = () => {
   return (
     <div className='w-full py-8 text-[#C4BCB2] font-[var] max-sm:py-2'>
-      <h1 className='text-[9.5vw] whitespace-nowrap uppercase tracking-wider max-sm:text-center'>Come back?</h1>
+      <h1 className='text-[9.5vw] whitespace-nowrap uppercase text-center tracking-wider'>Come back?</h1>
     </div>
   )
 }
